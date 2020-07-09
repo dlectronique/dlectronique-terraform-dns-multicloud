0e3c89e9a3b0f2c66386b9402f20ae694d15623c
dlectronique <dominique@hashicorp.com>
2020-07-09 18:41:32 +0100

    commented out gcp / azure in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   version = "~> 2.46.0"
   region  = var.aws_region
 }
-
+/*
 # GCP General Configuration
 provider "google" {
   version = "~> 3.5"
@@ -15,3 +15,4 @@ provider "google" {
 provider "azurerm" {
   version = "~>1.42.0"
 }
+*/
